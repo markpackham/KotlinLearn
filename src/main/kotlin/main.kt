5178a08d75442d8b6d44ab2d230e17e5ff857cb8
mark <markpackham1@gmail.com>
2020-11-10 12:32:56 +0000

    operations checks, https://youtu.be/5flXf8nuq60?t=1593

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,7 +1,36 @@
 // Learn Kotlin from https://www.youtube.com/watch?v=5flXf8nuq60
+// It's like Java for lazy people often used to make Android apps
 fun main(){
-    var x: Int = 3
+    // you can use "val" or "var" for variables, val is like "const" in JavaScript and var like "let"
+    // var x: Int = 3
+    // Kotlin is smart enough to identify some types like ints and longs
+    var mrLong = 3L
+    println("The value of x is $mrLong")
+    mrLong = 4
+    println("The value of x is now $mrLong")
+
+
+    // Variables
+//    val wholeNumber = 3
+//    val bigNumber = 3L
+//    val preciseDecimal = 3.33
+//    val decimalForMemoryCheapPeople = 3.33f
+//    val iLoveFootball = false
+//    val iLoveHockey = true
+//    val iAmALetter = 'a'
+
+    val x = 3f / 4f
+    val y = 10 % 2
+    val amIAnAdult = true
+    val amIAProgrammer = true
+    val amIAnAdultProgrammer = amIAnAdult && amIAProgrammer
+    val amIReallyAnAdultProgrammer = amIAnAdult || amIAProgrammer
+    val amIReallyReallyAnAdultProgrammer = amIAnAdult == amIAProgrammer
+    val amIReallyReallyReallyAnAdultProgrammer = !(amIAnAdult != amIAProgrammer)
     println("The value of x is $x")
-    x = 4
-    println("The value of x is now $x")
+    println("The value of y is $y")
+    println(amIAnAdultProgrammer)
+    println(amIReallyAnAdultProgrammer)
+    println(amIReallyReallyAnAdultProgrammer)
+    println(amIReallyReallyReallyAnAdultProgrammer)
 }

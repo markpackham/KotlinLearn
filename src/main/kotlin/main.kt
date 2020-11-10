dbc064dbecbecf81bf08440df43191bbe6325193
Martin Graves 422 <33223356+markpackham@users.noreply.github.com>
2020-11-10 12:10:08 +0000

    Learn Kotlin from https://www.youtube.com/watch?v=5flXf8nuq60

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,7 @@
+// Learn Kotlin from https://www.youtube.com/watch?v=5flXf8nuq60
+fun main(){
+    var x: Int = 3
+    println("The value of x is $x")
+    x = 4
+    println("The value of x is now $x")
+}
